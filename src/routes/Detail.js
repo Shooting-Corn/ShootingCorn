bf1829270d7309b336b317ffd993177264b6a992
heeseungYoo <yhs252525@gmail.com>
2021-03-23 06:12:13 +0900

    210323v compile ver

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -6,6 +6,33 @@ import styled from "styled-components";
 import Timeline from "./Timeline.js";
 import './Detail.css'
 
+
+//warning images
+import grade0 from './img/OK.gif'
+import violent1_a from './img/violent1_a.gif'
+import violent2_a from './img/violent2_a.gif'
+import violent3_a from './img/violent3_a.gif'
+
+
+import violent1 from './img/violent1.gif'
+import violent2 from './img/violent2.gif'
+import violent3 from './img/violent3.gif'
+
+
+import nudity1 from './img/nudity1.gif'
+import nudity2 from './img/nudity2.gif'
+import nudity3 from './img/nudity3.gif'
+
+
+import profanity1 from './img/profanity1.gif'
+import profanity2 from './img/profanity2.gif'
+
+
+import smoking from './img/smoking.gif'
+import drinking from './img/drinking.gif'
+
+
+
 const GET_MOVIE = gql`
   query getMovie($id: String!) {
     movie(id: $id) {
@@ -30,34 +57,103 @@ const GET_MOVIE = gql`
 
 const Container = styled.div`
 
-  width: 100vw;
-  height: 100vh;
+width: 100vw;
+height: 800px;
+maring-bottom:100px;
+padding-bottom:300px;
+
+
 `;
+
 
 
 const Column = styled.div`
 
-  width: 60%;
+position:absolute;
+top:600px;
+left:130px;
+
+  margin-left: 420px;
+  margin-bottom: 200px;
+  width: 50%;
   padding:40px;
-  margin-left: 40px;
+  
   background-color: white;
   border-radius: 18px;
   box-shadow:  10px 10px 10px #d1d2d4,
-               -10px -10px 10px #ffffff;  
+               -10px -10px 10px #ffffff;
+
+  
+  
 `;
 
 const Title = styled.h1`
   position:absolute;
-  left:550px;
-  top:200px;
+  width:800px;
+  height: auto;
+
+  left:500px;
+  top:250px;
   font-size: 85px;
   margin-bottom: 15px;
   color:red;
+  
+`;
+
+//nudity
+const Grade_nudity = styled.h4`
+  font-size: 85px;
+  width:10px;
+  position:absolute;
+  color:blue;
+  left:45px;
+  top:-40px;
+ 
+  z-index:2;
+`;
+
+//violent
+const Grade_violence = styled.h4`
+  font-size: 85px;
+  width:10px;
+  
+  position:absolute;
+  color:blue;
+  left:140px;
+  top:-40px;
+ 
+  z-index:2;
+`;
+
+//violent
+const Grade_profanity = styled.h4`
+  font-size: 85px;
+  width:10px;
+  
+  position:absolute;
+  color:blue;
+  left:235px;
+  top:-40px;
+  opacity: 0.8;
+  z-index:2;
+`;
+
+//violent
+const Grade_alcohol = styled.h4`
+  font-size: 85px;
+  width:10px;
+  
+  position:absolute;
+  color:blue;
+  left:330px;
+  top:-40px;
+  opacity: 0.8;
+  z-index:2;
 `;
 
 const Subtitle = styled.h4`
   margin-bottom: 35px;
-  font-size: 20px;
+  font-size: 18px;
 `;
 
 const Description = styled.p`
@@ -67,24 +163,30 @@ const Description = styled.p`
   color:black;
 `;
 
-
+// detail poster
 const Poster = styled.div`
-  width: 300px;
-  height: 450px;
+
+position:absolute;
+top: 0px; 
+left:-260px;
+  width: 30%;
+  height: 100%;
   background-color: transparent;
   background-image: url(${props => props.bg});
   background-size: cover;
+  background-position: center center;
+
   border-radius: 18px;
+
+  margin-bottom:100px;
 `;
 
-
-
-
+// background poster
 const Poster2 = styled.div`
 
-  position:absolute;
-  top: 0px; 
-  left:0px;
+position:absolute;
+top: 0px; 
+left:0px;
   width: 100%;
   height: 65%;
   background-color: transparent;
@@ -96,14 +198,8 @@ const Poster2 = styled.div`
   -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
   filter: grayscale(100%);
 
-  opacity:0.4
-
-
-
+  opacity:0.6
 `;
-
-
-
 
 const Nudity = styled.h4`
   font-size: 18px;
@@ -147,15 +243,111 @@ export default () => {
   return (
     <Container bg={data?.movie?.poster}>
       <Poster2 bg={data?.movie?.poster}></Poster2>
+      <center>
       <Title className="Title">
         {loading
           ? "Loading..."
           : `${data.movie.title} ${data.movie.isLiked ? "💖" : " "}`}
       </Title>
-      <div className="Wrapper">
-      <Poster bg={data?.movie?.poster}></Poster>
+
+      </center>
+
+      
+      
       <Column>
+
+      <Poster bg={data?.movie?.poster}></Poster>
         
+
+      
+
+
+
+      
+
+
+  <Grade_violence className="Grade_violence">
+
+{
+    
+    data?.movie?.grade === "All"
+    ?//All-grade violence
+    (data?.movie?.violence === 3
+      ? <img className ="grade_img" src={violent3_a}></img> //grade3
+      : ( data?.movie?.violence > 1
+          ? <img className ="grade_img" src={violent2_a}></img>  //grade2
+          : ( data?.movie?.violence === 0
+            ?<img className ="grade_img" src={grade0}></img>  //grade0
+            :<img className ="grade_img" src={violent1_a}></img>  //grade1
+            )
+        )
+    )
+
+    ://12+grade violence
+    (data?.movie?.violence === 3
+      ?<img className ="grade_img" src={violent3}></img>
+      : ( data?.movie?.violence > 1
+          ? <img className ="grade_img" src={violent2}></img>//2등급 
+          : ( data?.movie?.violence === 0
+            ?<img className ="grade_img" src={grade0}></img>//0등급
+            :<img className ="grade_img" src={violent1}></img> //1등급
+            )
+        )
+    )
+
+}
+
+</Grade_violence>
+
+
+
+<Grade_nudity className="Grade_nudity">
+
+{   
+  data?.movie?.nudity === 3
+    ?<img className ="grade_img" src={nudity3}></img> //nudity3
+    : (data?.movie?.nudity > 1
+        ? <img className ="grade_img" src={nudity2}></img>  ////nudity2
+        : ( data?.movie?.nudity === 0
+          ?<img className ="grade_img" src={grade0}></img>  //nudity0
+          :<img className ="grade_img" src={nudity1}></img>  ////nudity1
+          )
+      )
+  
+
+}
+</Grade_nudity>
+
+
+
+
+<Grade_profanity className="Grade_profanity">
+{   
+  data?.movie?.word > 7
+    ? <img className ="grade_img" src={profanity2}></img> //profanity2
+    : (data?.movie?.word === 0
+        ? <img className ="grade_img" src={grade0}></img>  ////profanity1
+        : <img className ="grade_img" src={profanity1}></img>  //profanity0
+      )
+}
+</Grade_profanity>
+
+
+
+<Grade_alcohol className="Grade_alcohol">
+
+{   
+  data?.movie?.alcohol === "no alcohol or smoking"
+    ?<img className ="grade_img" src={grade0}></img>  //grade0
+    : (data?.movie?.alcohol === "smoking and drinking"
+        ? <p><img className ="grade_img" src={smoking}></img> <img className ="grade_s_d" src={drinking}></img> </p>  //smoking and drinking
+        : ( data?.movie?.alcohol === "drinking"
+          ?<img className ="grade_img" src={drinking}></img>  //drinking
+          :<img className ="grade_img" src={smoking}></img>  ////smoking
+          )
+      )
+}
+</Grade_alcohol>
         <Subtitle className="Subtitle">
         {data?.movie?.grade}·{data?.movie?.genre}· {data?.movie?.directors} · {data?.movie?.runtime}min· {data?.movie?.stars}
         </Subtitle>
@@ -176,7 +368,7 @@ export default () => {
           onClose={closeModal}></Timeline>
         }
       </Column>
-      </div>
+  
     </Container>
   );
 };
